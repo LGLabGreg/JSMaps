badd11858cc46fc57be9903810367adbf94eb9bc
Gregory Lebret <greg@lglab.co.uk>
2016-04-13 16:49:14 +0100

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,112 @@
+
+/*!
+ * @description Grunt@!!!!!
+ */
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+	var map = grunt.option('map');
+
+  if(!map) {
+    grunt.fail.fatal('Missing "map" argument.');
+  }
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		clean: {
+      map: ['build/lg-map/' + map +'.js'],
+		  js: ['build/**/*.js','!build/lg-map/' + map +'.js'],
+      css: ['build/**/*.css'],
+      html: ['build/index.html'],
+      maps: ['build/maps']
+		},
+		watch: {
+			map: {
+		    files: ['src/maps/' + map +'.js'],
+		    tasks: ['clean:map', 'copy:map']
+		  },
+		  js: {
+		    files: ['Gruntfile.js','src/lg-map/*.js','!src/maps/' + map +'.js'],
+		    tasks: ['clean:js', 'copy:js']
+		  },
+		  css: {
+		    files: 'src/**/*.css',
+			  tasks: ['clean:css', 'copy:css']
+		  },
+		  html: {
+		    files: 'src/**/*.html',
+			  tasks: ['clean:html', 'copy:html']
+		  }
+		},
+		copy: {
+			map: {
+        files: [{
+          nonull: true,
+          cwd: 'src/maps/',
+          expand: true,
+          src: map +'.js',
+          dest: 'build/lg-map/',
+          filter: 'isFile',
+          flatten: true
+        }]
+      },
+      js: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['**/*.js', '!maps/' + map +'.js'],
+          dest: 'build'
+        }]
+      },
+      css: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['**/*.css'],
+          dest: 'build'
+        }]
+      },
+      html: {
+        options: {
+          process: function (content) {
+            return grunt.template.process(content, {
+              data: grunt.util._.extend({
+                map: 'lg-map/' + map + '.js'
+              }, {})
+            });
+          }
+        },
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['index.html'],
+          dest: 'build'
+          
+        }]
+      }
+    },
+    connect: {
+      server: {
+        options: {
+        keepalive: false,
+        base: 'build/',
+        hostname: 'localhost',
+        port: 8000,
+        options: {
+          index: 'index.html',
+          maxAge: 300000
+        }
+        }
+      }
+    }
+	});
+
+	grunt.registerTask('development', ['clean', 'copy', 'clean:maps', 'connect:server', 'watch']);
+	grunt.registerTask('build', ['clean', 'copy', 'clean:maps']);
+
+};
