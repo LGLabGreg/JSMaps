a2c4f38ce2eda66af15a2cbc048b56202954ac79
Gregory Lebret <greg@lglab.co.uk>
2017-06-11 10:58:37 +0100

    Added Japan, Egypt and North Africa

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/saudiArabia.json", function(json) {
+  $.getJSON("maps/north-africa.json", function(json) {
 
     var states = json.svg.g.path;
 
