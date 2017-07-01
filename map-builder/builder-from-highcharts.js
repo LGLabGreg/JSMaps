6f86c70214399037bd049ac4359efb4f2f5be690
Gregory Lebret <greg@lglab.co.uk>
2017-07-01 10:35:34 +0100

    Added venezuela

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/lodon.json", function(json) {
+  $.getJSON("maps/venezuela.json", function(json) {
 
     var states = json.svg.g.path;
 
