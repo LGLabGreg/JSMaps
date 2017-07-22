beeb3ff44cf82d6000aaa55ab2d88ec633e5a10b
Gregory Lebret <greg@lglab.co.uk>
2017-07-22 15:03:39 +0100

    Luxembourg

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/slovekia.json", function(json) {
+  $.getJSON("maps/luxembourg.json", function(json) {
 
     var states = json.svg.g.path;
 
