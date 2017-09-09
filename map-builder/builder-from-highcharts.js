e0e30999b552d6f171f3559d2ff56697060cca24
Gregory Lebret <greg@lglab.co.uk>
2017-09-09 14:20:17 +0100

    more maps

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/angola.json", function(json) {
+  $.getJSON("maps/sriLanka.json", function(json) {
 
     var states = json.svg.g.path;
 
