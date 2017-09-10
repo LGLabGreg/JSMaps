ef38eafed7143ed83cb45fed4459034f5f268b7e
Gregory Lebret <greg@lglab.co.uk>
2017-09-10 15:09:09 +0100

    add usa with territories

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/sriLanka.json", function(json) {
+  $.getJSON("maps/usaTerritories.json", function(json) {
 
     var states = json.svg.g.path;
 
