224247282a26edd86c75307258ea6c6d03aabfe0
LGLabGreg <greg@lglab.co.uk>
2017-10-19 07:54:10 +0100

    maps

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/pennsylvania.json", function(json) {
+  $.getJSON("maps/georgia.json", function(json) {
 
     var states = json.svg.g.path;
 
