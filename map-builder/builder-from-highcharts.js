5f1cc3ab9582e91bd3a91976f5ecb059bc9830b3
Gregory Lebret <greg@lglab.co.uk>
2017-10-19 09:10:00 +0100

    add michigan

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/georgia.json", function(json) {
+  $.getJSON("maps/michigan.json", function(json) {
 
     var states = json.svg.g.path;
 
