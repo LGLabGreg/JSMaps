dc39b7756d3ee77d283c0f8bf10848e26b514ee6
Gregory Lebret <greg@lglab.co.uk>
2017-11-03 17:03:43 +0000

    added missouri

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/tennessee.json", function(json) {
+  $.getJSON("maps/missouri.json", function(json) {
 
     var states = json.svg.g.path;
 
