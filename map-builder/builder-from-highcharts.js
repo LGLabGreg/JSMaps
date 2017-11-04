44479e8f95e056d92d1c0f015dbf06cf1e6ecf2d
Gregory Lebret <greg@lglab.co.uk>
2017-11-04 11:26:33 +0000

    maps

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/missouri.json", function(json) {
+  $.getJSON("maps/tanzania.json", function(json) {
 
     var states = json.svg.g.path;
 
