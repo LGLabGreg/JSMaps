69e2cecbd4f93538010da408617d454e9c8a0c36
Gregory Lebret <greg@lglab.co.uk>
2017-11-18 16:28:20 +0000

    add zimbabwe

--- map-builder/builder-from-highcharts.js
+++ map-builder/builder-from-highcharts.js
@@ -9,7 +9,7 @@ $(function() {
   var totalColors = colors.length;
   var newPaths = [];
 
-  $.getJSON("maps/elSalvador.json", function(json) {
+  $.getJSON("maps/zimbabwe.json", function(json) {
 
     var states = json.svg.g.path;
 
